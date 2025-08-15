0ce2113275d6a7a9b470e0cd5c4b47ed2c4e29c4
dejithedev <onesixteenj@gmail.com>
2025-08-15 18:35:27 +0100

    added api url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api/',
+    baseURL: import.meta.env.VITE_API_URL + '/api/',
     withCredentials: true,
 });
